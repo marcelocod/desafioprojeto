e42e6033684b47009a3de62a5f0b517ed4acd917
marcelocode <marcelodasilvalisboa1@gmail.com>
2024-07-02 13:26:00 -0300

    Desafio clasificador de Nivel de Herói

--- pastinha/index.js
+++ pastinha/index.js
@@ -1,2 +1,30 @@
-let nome = "Marcelo"
-console.log("Meu nome é: " + nome )
+// Variável para o nome do herói
+let nomeDoHeroi = "Aatrox";
+
+// Variável para a quantidade de XP
+let xpDoHeroi = 12000;
+
+// Função para determinar o nível do herói com base na XP
+function determinarNivelHeroi(xp) {
+    if (xp < 1000) {
+      return "Ferro";
+    } else if (xp >= 1000 && xp < 2000) {
+      return "Bronze";
+    } else if (xp >= 2000 && xp < 5000) {
+      return "Prata";
+    } else if (xp >= 5000 && xp < 7000) {
+      return "Ouro";
+    } else if (xp >= 7000 && xp < 8000) {
+      return "Platina";
+    } else if (xp >= 8000 && xp < 9000) {
+      return "Ascendente";
+    } else {
+      return "Radiante";
+    }
+  }
+  
+  // Exibindo o nível do herói
+  let nivelHeroi = determinarNivelHeroi(xpDoHeroi);
+  console.log(`O herói ${nomeDoHeroi} está no nível ${nivelHeroi}`);
+  
+
